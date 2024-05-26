7d061c040e533248d34a118a2a85adcf14bec58e
amex77 <ashishmishraofficial@outlook.com>
2024-05-26 20:58:33 +0530

    Expressions #Day 1

---
+++ 05_expression conditionals/index.js
@@ -0,0 +1,64 @@
+console.log("Expression in Js")
+
+// Arithmetic Operators
+
+let a = 48;
+let b = 5;
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a * b = ", a * b)
+console.log("a / b = ", a / b)
+console.log("a ** b = ", a ** b)
+console.log("a % b = ", a % b)
+console.log("a++ = ", a++)
+
+// Assignment Operators
+
+
+let new1 = 2;
+
+console.log(new1)
+new1 **= 10;
+console.log(new1);
+
+new1 += 10;
+console.log(new1)
+
+new1 += 10;
+console.log(new1)
+
+new1 -= 10;
+console.log(new1)
+
+new1 /= 10;
+console.log(new1)
+
+new1 %= 10;
+console.log(new1)
+
+// Comparison Operators
+
+let comp1 = 6;
+let comp2 = "7";
+console.log("comp1 == comp2 is", comp1 == comp2)
+console.log("comp1 != comp2 is", comp1 != comp2)
+console.log("comp1 === comp2 is", comp1 === comp2)
+console.log("comp1 !== comp2 is", comp1 !== comp2)
+console.log("comp1 > comp2 is", comp1 > comp2)
+console.log("comp1 < comp2 is", comp1 < comp2)
+console.log("comp1 >= comp2 is", comp1 >= comp2)
+console.log("comp1 <= comp2 is", comp1 <= comp2)
+
+// Logical Operators
+let x = 5;
+let y = 6;
+console.log(x < y && x == 5)
+console.log(x > y || x == 5)
+console.log(!false)
+
+
+
+
+
+
+
