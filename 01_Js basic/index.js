93c0815a3a4748d1735abc7c8a9f13c8bd797bfd
amex77 <ashishmishraofficial@outlook.com>
2024-05-26 19:45:57 +0530

    Javascript Basics #Day1

---
+++ 01_Js basic/index.js
@@ -0,0 +1,26 @@
+console.log("This is the first Javascript Program");
+
+// This is how you add a commment in Javascript
+
+//Varibale Declaration via let
+
+let a = 10;
+console.log(a);
+
+a='Super User';
+console.log(a);
+
+//Dynamically Typed language Javascipt
+
+let $a = 100;
+console.log($a);
+
+let _a = 200;
+console.log(_a);
+
+//Rules for choosing variable names in JS
+//Letters, digits, underscores and $ sign allowed
+//Must begin with a $, _ or a letter 
+//Javascript reserved words cannot be used as a variable name forexample 
+
+//let var = 10;
