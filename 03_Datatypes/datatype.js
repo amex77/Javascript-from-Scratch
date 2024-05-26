48a6c63b2cb9e87ce2cf964788ce9206a2ce5cf0
amex77 <ashishmishraofficial@outlook.com>
2024-05-26 20:18:13 +0530

    JS Datatypes #Day1

---
+++ 03_Datatypes/datatype.js
@@ -0,0 +1,22 @@
+// nn bb ss u - Primitives in Js
+let a = null;
+let b = 345;
+let c = true; // can also be false
+let d = BigInt("567") + BigInt("3") 
+let e = "Harry"
+let f = Symbol("I am a nice symbol")
+let g
+console.log(a, b, c , d, e, f, g)
+console.log(typeof c)
+
+// Non Primitive Data Type - Objects in Js
+const item = {
+  "$uper_User": true,
+  "Dj": false,
+  "Kashish ": 69,
+  "solitude": undefined,
+  "new": "sobriety"
+}
+console.log(item["$uper_User"])
+
+item.$uper_User = false;
