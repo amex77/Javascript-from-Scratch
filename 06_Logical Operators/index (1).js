3a588fbe442b7a79d2951a6bf20c15b16af6926d
amex77 <ashishmishraofficial@outlook.com>
2024-05-26 23:28:10 +0530

    Logical Operators #Day 1

---
+++ 06_Logical Operators/index (1).js
@@ -0,0 +1,16 @@
+//logical operators
+//&&
+
+let a = 5;
+let b = 6;
+
+console.log(a<b && a==5)
+console.log(a>b && a==5)
+
+//||
+console.log(a<b || a==5)
+console.log(a>b || a==5)
+console.log(a==6 || b<5)
+
+// !
+console.log(!false)
