9c25ab5708e6a59e47aa89838a032e271bac58e6
amex77 <ashishmishraofficial@outlook.com>
2024-05-27 10:46:16 +0530

    Conditional Statements C2 #Day 2

--- 07_Conditional Statme/index.js
+++ 07_Conditional Statme/index.js
@@ -1,11 +1,18 @@
 // conditional statements
 // if statement
-const prompt=require("prompt-sync")();
+const prompt = require("prompt-sync")();
 let a = prompt("Hey whats your age?");
 a = Number.parseInt(a); // Converting the string to a number
 
+if (a > 18) {
+  console.log("You are eligible to drive");
+} else if (a < 18) {
+  console.log("You are NOT eligible to drive");
+} else {
+  console.log("Invalid Input");
+}
 
 // ternary operator
 
 let age = prompt("Hey whats your age?");
-console.log("You ", (age<18? "can vote" : "cannot vote"));
+console.log("You ", age < 18 ? "can vote" : "cannot vote");
