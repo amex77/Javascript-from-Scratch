b3f8f9ec13932b788c8076d04636c9e8018881e4
amex77 <ashishmishraofficial@outlook.com>
2024-05-28 11:37:34 +0530

    while loops C2 #Day 3

--- 12_While Loops/index.js
+++ 12_While Loops/index.js
@@ -21,19 +21,35 @@ do {
     i++
 }while(i<a) */
 
-
 //do while looop is  a type of loop which no matter it execute once
 
-const prompt=require("prompt-sync")();
-let a=prompt("Enter a number");
-a=Number.parseInt(a);
-let i=1;
+//Program to print n natural numbers using while loop
 
-let sum=0;
-while(i<=a) {
-    sum += i;
-    i++;
-}
+/* const prompt = require("prompt-sync")();
+let a = prompt("Enter a number");
+a = Number.parseInt(a);
+let i = 1;
+
+let sum = 0;
+while (i <= a) {
+  sum += i;
+  i++;
+} 
 
 console.log(sum);
+*/
 
+//Program to print n natural numbers using do while loop
+
+const prompt = require("prompt-sync")();
+let a = prompt("Enter a number");
+a = Number.parseInt(a);
+let i = 1;
+
+let sum = 0;
+do {
+  sum += i;
+  i++;
+}while (i <= a);
+
+console.log(sum);
