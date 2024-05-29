8ff60c563c97a9a64403aa91d60de48ce1cd81a9
amex77 <ashishmishraofficial@outlook.com>
2024-05-29 12:52:13 +0530

    Strings #Day4

---
+++ 15_Strings/index.js
@@ -0,0 +1,33 @@
+/* let name = "Ashish";
+console.log(name.length)
+
+console.log(name[0]);                               
+console.log(name[4]);
+console.log(name[5]);
+ */
+
+//Template Literals
+
+let girl1 = "Neha";
+let girl2 = "Riya";
+
+let sentence = `${girl1} is a friend of ${girl2}`;
+
+console.log(`${girl1} is printing a string using backticks`);
+console.log(sentence);
+
+console.log(
+  `Here's an example of using both double and single quotes: "I said 'hello'".`
+);
+
+//Escape sequence Characters
+
+let name = "Ashish'Mishra";
+console.log(name); //Output: Ashish'Mishra
+
+console.log("Hello\nWorld"); // Output: "Hello
+// World"
+
+console.log("Hello\rWorld"); // Output: "World"
+
+console.log("Hello\tWorld"); //Output:Hello   World
