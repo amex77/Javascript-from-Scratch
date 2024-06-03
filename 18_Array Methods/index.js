8b50d96ee3d5704c178bde55a240cdc2e4e1b542
amex77 <ashishmishraofficial@outlook.com>
2024-06-03 21:19:44 +0530

    Arrays Methods #Day6

---
+++ 18_Array Methods/index.js
@@ -0,0 +1,25 @@
+// Array methods
+
+let num = [1, 3, 6, 9];
+let a = num.toString();
+console.log(a);
+console.log(typeof a);
+
+let c = num.join(" and ");
+console.log(c, typeof c);
+
+
+let p = num.pop();
+console.log(num, p);
+
+//pop returns the popped element
+
+// let r = num.push(56) // push returns the new array length
+// console.log(num, r)
+
+// let r = num.shift()
+// console.log(r, num) // Removes an element from the start of the array
+
+let r = num.unshift(78)
+console.log(r, num)
+console.log(r)
