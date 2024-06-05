b1f6bdd879465ea6cc4a091bf4ad5bf7f7983af5
amex77 <ashishmishraofficial@outlook.com>
2024-06-05 18:14:35 +0530

    Array Methods2 #Day7

---
+++ 19_Array Methods2/index.js
@@ -0,0 +1,32 @@
+/* let num = [1, 3, 7 ,9];
+let num2 = [611, 72, 91 ,'string'];
+
+let papanum =(num.concat(num2));
+console.log(papanum);
+
+
+let mamanum = (num2.sort());
+console.log(mamanum)
+ */
+//sort function sorts the arrys 'alphabetically'
+//sort functions modifies the main array
+ 
+
+//splice 
+
+/* latest = [ 11, 12 , 13, 14, 15]
+console.log(latest.splice(1, 3, 112, 113 ,114));
+console.log(latest); */
+
+
+/* Syntax
+splice(start, deleteCount, item1, item2) */
+
+
+//slice
+
+latestnew = [ 101, 162 , 143, 14, 15]
+let kaleshan = latestnew.slice(1);
+console.log(kaleshan);
+let kaleshan2 = kaleshan.slice (2, 5);
+console.log(kaleshan2);
