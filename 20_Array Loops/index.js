b6a4e08a2a238e1712913fffcf47d658b9cc87f2
amex77 <ashishmishraofficial@outlook.com>
2024-06-08 00:00:05 +0530

    Array Loops C2 #Day8

--- 20_Array Loops/index.js
+++ 20_Array Loops/index.js
@@ -1,5 +1,15 @@
 let num = [3, 5, 1, 2, 4]
 
-for(let i=0; i<num.length;i++){
+/* for(let i=0; i<num.length;i++){
 console.log(num[i])
- }
+ } */
+
+// ForEach Loop
+num.forEach((element) => {
+    console.log(element * element)
+  })
+  
+  // Array.from 
+  let name = "Harry"
+  let arr = Array.from(name)
+  console.log(arr)
